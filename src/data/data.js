9c6c3a739f40472c7fe9706ccfd30fca44f8f086
IPaul <ipaul@NY-COU-IPAUL.turner.com>
2020-12-08 18:33:57 -0500

    Add video data file

---
+++ src/data/data.js
@@ -0,0 +1,158 @@
+const data = [
+  {
+    theme: "Family",
+    video: "https://vimeo.com/434733209",
+    poster: "",
+    brand: "Target",
+    title: "How One Dad Started a Movement to Celebrate Black Fatherhood",
+    description:
+      "The Dad Gang began in 2016 as an Instagram account designed to put positive images of Black fatherhood out into the world. Today, the Dad Gang is an entire social community made up of Black dads who support and celebrate each other. The Dad Gang reminds us to love, cherish and protect these men who want nothing more than to be there for their kids.",
+    quote: "",
+  },
+  {
+    theme: "Equality",
+    video: "https://vimeo.com/412796207",
+    poster: "",
+    brand: "P&G/Secret",
+    title: "You Can't Pause Progress",
+    description:
+      "Secret is proud to support women who are “all strength, no sweat” – those who boldly fight for progress not only for themselves but for all women, without sweating the obstacles in their path. Secret Deodorant is partnering with Girls Leading Girls to keep young female athletes across America stay physically active and mentally motivated during these difficult times.",
+    quote: "",
+  },
+  {
+    theme: "Beauty",
+    video: "https://vimeo.com/376857392",
+    poster: "",
+    brand: "Roberto Coin",
+    title: "Inspired By Beauty",
+    description:
+      "Italian jewelry designer Roberto Coin has spent his life searching for beauty. A self-styled ‘beauty explorer,’ Roberto asserts that the most important experiences of beauty cannot be seen or even touched, they must be felt with the heart. That sentiment imbues every piece of jewelry he creates.",
+    quote: "",
+  },
+  {
+    theme: "Innovation",
+    video: "https://vimeo.com/434734374",
+    poster: "",
+    brand: "Pfizer",
+    title: "Relentless Pursuit: No Stone Left Unturned",
+    description:
+      "Right now, all around the world, the medical community is working tirelessly to treat and prevent COVID-19. Among them are thousands of Pfizer employees, each pouring energy, curiosity and hope into daily scientific pursuit. Meet the scientists who have become first-line responders racing against time to halt the spread of COVID-19. This is more than just a job for Pfizer employees, this is a calling and passion and they will leave no stone unturned in their pursuit for a vaccine.",
+    quote: "",
+  },
+  {
+    theme: "Perseverance",
+    video: "https://vimeo.com/430464732",
+    poster: "",
+    brand: "Charles Schwab",
+    title: "Breakaway",
+    description:
+      "Seasoned investor and Charles Schwab customer, Bill Greenberg and his family live in New York City. When his son Sam was born with a rare and undiagnosed illness, Bill and his wife struggled through years of uncertainty before finding solace through the unexpected outlet of competitive sports. After seeing his son fall in love with the specialist sport of sled-hockey, Bill helped found the New York Sled Rangers, which today connects over 100 players, helping bring confidence and independence to disabled children and a community of support for parents.",
+    quote: "",
+  },
+  {
+    theme: "Dedication",
+    video: "https://vimeo.com/442091766",
+    poster: "",
+    brand: "Invesco",
+    title: "Basketball is Back",
+    description:
+      "Basketball is back with an innovative new format that promises to get fans up and cheering. To celebrate, Invesco QQQ is excited to share the story of HomeCourt, a top basketball app that uses AI and augmented reality to help basketball players of all skill levels train smarter from the comfort of their own homes. Let’s get back in the game and innovate play!",
+    quote: "",
+  },
+  {
+    theme: "Stewardship",
+    video: "https://vimeo.com/383798189",
+    poster: "",
+    brand: "John Hancock",
+    title: "Who's Saving the Maah Daah Hey?",
+    description:
+      "The Maah Daah Hey Trail is 144 miles of single track trail running through the Badlands of North Dakota. For Nick and Lindsey Ybarra, the Maah Daah Hey is more than just a trail, it’s a place where people come together. After seven years of maintaining the trail and running the Maah Daah Hey 100 bike race with the help from loyal volunteers, the Ybarra family remains more committed than ever to preserving this pristine piece of wilderness. Together with John Hancock, see how the Ybarras are reimagining wealth and making an impact on their community.",
+    quote: "",
+  },
+  {
+    theme: "Service",
+    video: "https://vimeo.com/470720637",
+    poster: "",
+    brand: "USAA",
+    title: "Gold Star Families - Town name begins with 'E'",
+    description:
+      "Losing an active duty parent is one of the biggest challenges a military kid can face. That’s why veteran Perry Yee started Active Valor, a program that pairs Gold Star kids with veterans - providing children with mentorship and veterans with a new sense of purpose.",
+    quote: "",
+  },
+  {
+    theme: "Mobility",
+    video: "https://vimeo.com/404740466",
+    poster: "",
+    brand: "NAR",
+    title: "Home is Where You Park It",
+    description:
+      "After 2.5 years living in a camper in California to save money, The Gelle family of 5 knew it was time to put down some roots and purchase a home back in Virginia. They relied upon their old friend, and REALTOR®, Linda Batten to help them find their new home from all the way across the country. Batten & the Gelles have forged much more than just a realtor/home buyer bond – they come from military families who have a deep understanding of looking out for one another no matter the circumstance. “Wherever they laid their roots, that’s what home is and while they are protecting & serving us, I want them to have a safe place in which to grow their family.”",
+    quote: "",
+  },
+  {
+    theme: "Caregiving",
+    video: "https://vimeo.com/441078391",
+    poster: "",
+    brand: "Sleep Number",
+    title: "A North Country Midwife",
+    description:
+      "Sunday Smith is the only midwife for 100 miles (in any direction) from her home in New York’s North Country. Every morning, Sunday wakes up at 5am and quickly gets on the road to see her 4-8 clients per day. She has roughly 25-45 women in her care at any given time, and spends much of her waking life devoted to their wellbeing. Her hours are such that her husband recently left his job to stay at home with their two children. Because she’s on call 24/7, sleep is crucial for Sunday. Midwifery is both mentally and physically demanding, and without sleep, she knows she can’t offer the critical services her clients need.",
+    quote: "",
+  },
+  {
+    theme: "Progress",
+    video: "https://vimeo.com/487358399",
+    poster: "",
+    brand: "AT&T",
+    title: "Forces of Change: The Black Scientific Rennaissance",
+    description:
+      "In the 1960s and 1970s, microphone inventor Dr. James West led a Black Scientific Renaissance at Bell Labs that helped launch the careers of hundreds of successful Black scientists. Today, his daughter Ellington West carries on his legacy as CEO of a medical device and software company working at the cutting edge of artificial intelligence. “As the next generation, we really need to face this as our own renaissance,” says Ellington. See how she and her father are part of an inspiring movement toward greater equity and inclusion.",
+    quote: "",
+  },
+  {
+    theme: "Fun - Please update value",
+    video: "https://vimeo.com/453763150",
+    poster: "",
+    brand: "Lego",
+    title: "Movie Stunts/Lego Cars",
+    description:
+      "LEGO Technic’s building system of beams, gear wheels, and axles is inspired by the real world. Who better to size up the realistic, working features of Technic than someone with a close, intimate, life-or-death relationship with motor vehicles? That’s where our veteran stunt person, Rick Fike, comes in!",
+    quote: "",
+  },
+  {
+    theme: "",
+    video: "",
+    poster: "",
+    brand: "",
+    title: "",
+    description: "",
+    quote: "",
+  },
+  {
+    theme: "",
+    video: "",
+    poster: "",
+    brand: "",
+    title: "",
+    description: "",
+    quote: "",
+  },
+  {
+    theme: "",
+    video: "",
+    poster: "",
+    brand: "",
+    title: "",
+    description: "",
+    quote: "",
+  },
+  {
+    theme: "",
+    video: "",
+    poster: "",
+    brand: "",
+    title: "",
+    description: "",
+    quote: "",
+  },
+];
