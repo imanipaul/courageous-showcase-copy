f22a71f05a2ac3fd2fd677f303e6362b08149cfb
IPaul <ipaul@NY-COU-IPAUL.turner.com>
2020-12-08 16:29:44 -0500

    Add icon assets

--- src/App.js
+++ src/App.js
@@ -2,21 +2,8 @@ import "./App.css";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 
 import Video from "./Components/Video";
-import Test from "./Test";
 
 function App() {
-  // const pickArticle = () => {
-  //   if (
-  //     window.location.pathname.split("/").includes("InvestmentGradeCashmere")
-  //   ) {
-  //     return <ArticleOne />;
-  //   }
-  //   if (window.location.pathname.split("/").includes("SecuringTheWorld")) {
-  //     return <ArticleTwo  />;
-  //   }
-  //   return <ArticleOne />;
-  // };
-
   return (
     <HelmetProvider>
       <div className="App">
