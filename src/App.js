5af4ff0c1a67f71478054c380bcc6e82692d9e03
IPaul <ipaul@NY-COU-IPAUL.turner.com>
2020-12-08 13:46:38 -0500

    Install node sass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import Test from './Test'
 
 function App() {
+
+  // const pickArticle = () => {
+  //   if (
+  //     window.location.pathname.split("/").includes("InvestmentGradeCashmere")
+  //   ) {
+  //     return <ArticleOne />;
+  //   }
+  //   if (window.location.pathname.split("/").includes("SecuringTheWorld")) {
+  //     return <ArticleTwo  />;
+  //   }
+  //   return <ArticleOne />;
+  // };
+
   return (
     <div className="App">
-     Showcase
+This is the App Page
+    {window.location.pathname.split("/").includes('test') && <Test />}
+     
     </div>
   );
 }
