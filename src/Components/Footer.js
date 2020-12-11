b6019d1e4db5a9da14902cdf5a1f5cbd2f5c556d
IPaul <ipaul@NY-COU-IPAUL.turner.com>
2020-12-11 10:47:39 -0500

    Link footer back to home

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ import "../styles/Footer.scss";
 
 export default function Footer() {
   return (
-    <footer>
+    <footer onClick={() => (window.location = `/`)}>
       <p>
         <span className="first">Courageous Showcase </span>
         <span className="last">2020 Values</span>
