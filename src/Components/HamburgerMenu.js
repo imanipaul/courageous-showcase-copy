a0b3404f28f99063dc434ac589ffd59c31427297
IPaul <ipaul@NY-COU-IPAUL.turner.com>
2020-12-11 11:11:49 -0500

    Add icon to who we are on hamburger menu

--- src/Components/HamburgerMenu.js
+++ src/Components/HamburgerMenu.js
@@ -1,16 +1,19 @@
 import { slide as Menu } from "react-burger-menu";
 import "../styles/HamburgerMenu.scss";
 import close from "../assets/images/Close.svg";
+import arrow from "../assets/images/RIGHT-ARROW.svg";
 
 export default function HamburgerMenu(props) {
   return (
     <section
       className={`burger-menu-container ${props.visible ? "show" : "hidden"}`}
-      //   className={`burger-menu-container hidden`}
     >
       <nav>
         <img src={close} alt="close" onClick={() => props.setVisible(false)} />
-        <a>Home</a>
+        <a>
+          Who we are
+          <img src={arrow} alt="arrow" />
+        </a>
         <a>Vimeo</a>
         <a>LinkedIn</a>
         <a>Instagram</a>
